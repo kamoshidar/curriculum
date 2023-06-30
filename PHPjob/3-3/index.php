9c54e1197d218b0ca6c3a2bfbf05365bc466e0d1
kamoshidar <108622375+kamoshidar@users.noreply.github.com>
2023-06-30 23:35:00 +0900

    Create index.php

---
+++ PHPjob/3-3/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Practice2</title>
+</head>
+<body>
+ <form action="result.php" method="POST">
+ 好きな名前を入れてください<br>
+ <input type="text" name="name" /><br>
+ <br>
+ 1~6の中で好きな数字を入れてください<br>
+ <input type="text" name="number" /><br>
+<br>
+ <input type="submit" value="送信"/><br>
+
+  </form>
+</body>
+</html>
