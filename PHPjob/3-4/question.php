a1f47e6feb8fb0571d673005e03542a748fc92dc
kamoshidar <108622375+kamoshidar@users.noreply.github.com>
2023-07-18 01:40:11 +0900

    Update question.php

--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -2,12 +2,12 @@
 $name = $_POST['name'];
 // ①画像を参考に問題文の選択肢の配列を作成してください。
 $array = [
-    [80, 22, 20, 21],
+    ["80", "22", "20", "21"],
     ["PHP", "Python", "JAVA", "HTML"],
     ["join", "select", "insert", "update"]
 ];
 // ② ①で作成した、配列から正解の選択肢の変数を作成してください
-$array2 = [80, "PHP", "update"];
+$array2 = ["80", "PHP", "select"];
 ?>
 
 <!DOCTYPE html>
@@ -19,40 +19,29 @@ $array2 = [80, "PHP", "update"];
     <title>クイズ</title>
     <link rel="stylesheet" href="index.css">
 
-    <style>
-        body{
-        background-color: gray;
-        align-items: center;
-        justify-content: center;
-    color: white;
-        }
-    </style>
 </head>
 
 
 <body class="answer">
 <form action="answer.php" method="post">
     <p>お疲れ様です<?php echo $name; ?>さん</p>
-    <!--フォームの作成 通信はPOST通信で-->
-    <h2>①ネットワークのポート番号は何番？</h2>
-    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-    <?php
+    <!--フォーム2の作成 通信はPOST通信で-->
+    <?php echo "<h2>①ネットワークのポート番号は何番？</h2>\n";
+    //<--③ 問題のradioボタンを「foreach」を使って作成する-->
     foreach ($array[0] as $value) {
-        echo "<input type=\"radio\" name=\"port\" value=" . $value . ">" . $value;
+        echo "<input type=\"radio\" name=\"port\" value=" . $value . ">".$value;
     }
     ?>
 
-    <h2>②Webページを作成するための言語は？</h2>
-    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-    <?php
+    <?php echo "<h2>②Webページを作成するための言語は？</h2>\n";
+    //<!--③ 問題のradioボタンを「foreach」を使って作成する-->
     foreach ($array[1] as $value) {
         echo "<input type=\"radio\" name=\"web\" value=" . $value . ">" . $value;
     }
     ?>
 
-    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
-    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-    <?php
+    <?php echo "<h2>③MySQLで情報を取得するためのコマンドは？</h2>\n";
+    //<!--③ 問題のradioボタンを「foreach」を使って作成する-->
     foreach ($array[2] as $value) {
         echo "<input type=\"radio\" name=\"sql\" value=" . $value . ">" . $value;
     }
@@ -66,7 +55,3 @@ $array2 = [80, "PHP", "update"];
 </form>
 </body>
 </html>
-
-
-
-
